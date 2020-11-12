382a91ad77c4a8b7f8e0c5c5e9327ecd9923011e
Daniel Popov <popovdaniil2000@gmail.com>
2020-11-12 21:37:40 +0300

    add args

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,24 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ArchiverException {
-        String pathFile = "C:\\Users\\Work\\IdeaProjects\\Archivator\\Test"; // Путь к файлу или папке для архивации
+        if (args.length == 3){
+            // args[0] - Путь к файлу или папке для архивации
+            // args[1] - Место создания архива
+            // args[2] - Название архива
+            Archiver.compress(args[0],args[1],args[2]);
+        } else if (args.length == 2){
+            // args[0] - Путь к архиву
+            // args[1] - Путь к месту разархивации
+            Archiver.extract(args[0],args[1]);
+        }
+
+       /* String pathFile = "C:\\Users\\Work\\IdeaProjects\\Archivator\\Test"; // Путь к файлу или папке для архивации
         String outDir ="C:\\Users\\Work\\Documents\\Tests"; // Место создания архива
         String name="Test";//Название архива
         Archiver.compress(pathFile,outDir,name);
 
         String Archive = "C:\\Users\\Work\\Documents\\Tests\\TestZIP"; // Путь к архиву
         String out="C:\\Users\\Work\\Documents\\матан"; // Путь к месту разархивации
-        Archiver.extract(Archive,out);
+        Archiver.extract(Archive,out);*/
     }
 }
